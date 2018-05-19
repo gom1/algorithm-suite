27245c15c9a5db2a01e420ae632afea4b4f56d61
German Om <germanom91@gmail.com>
2018-05-18 22:29:48 -0700

    adding tests for method that sorts a list with insertion sort

--- tests/test_insertion_sort.py
+++ tests/test_insertion_sort.py
@@ -1,5 +1,5 @@
 import unittest
-from algorithms.insertion_sort import insertion_sort
+from algorithms.insertion_sort import insertion_sort, sort_with_insertion_sort
 
 
 class TestInsertionSort(unittest.TestCase):
@@ -44,6 +44,39 @@ class TestInsertionSort(unittest.TestCase):
         expected_list = [1, 2, 3, 4, 4, 5]
         result = insertion_sort(initial_list, x)
         self.assertEqual(expected_list, result)
+
+
+class TestSortWithInsertionSort(unittest.TestCase):
+    def test_empty_list(self):
+        initial_list = []
+        expected_list = []
+        result = sort_with_insertion_sort(initial_list)
+        self.assertEqual(expected_list, result)
+
+    def test_already_sorted_list(self):
+        initial_list = [1, 2, 3]
+        expected_list = [1, 2, 3]
+        result = sort_with_insertion_sort(initial_list)
+        self.assertEqual(expected_list, result)
+
+    def test_mixed_integers_list(self):
+        initial_list = [1, 5, 2]
+        expected_list = [1, 2, 5]
+        result = sort_with_insertion_sort(initial_list)
+        self.assertEqual(expected_list, result)
+
+    def test_edge_case_sort_whole_list(self):
+        initial_list = [4, 3, 2, 1]
+        expected_list = [1, 2, 3, 4]
+        result = sort_with_insertion_sort(initial_list)
+        self.assertEqual(expected_list, result)
+
+    def test_edge_case_duplicate_integers_in_list(self):
+        initial_list = [8, 2, 5, 3, 4, 5]
+        expected_list = [2, 3, 4, 5, 5, 8]
+        result = sort_with_insertion_sort(initial_list)
+        self.assertEqual(expected_list, result)
+
 
 
 if __name__ == '__main__':
