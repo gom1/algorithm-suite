6702a524811e1f5bf050ac5d38ff9c631d674f1e
German Om <germanom91@gmail.com>
2018-05-19 16:09:23 -0700

    refactor import statement in tests

--- tests/test_insertion_sort.py
+++ tests/test_insertion_sort.py
@@ -1,5 +1,5 @@
 import unittest
-from algorithms.insertion_sort import insertion_sort, sort_with_insertion_sort
+import algorithms.insertion_sort as ais
 
 
 class TestInsertionSort(unittest.TestCase):
@@ -7,42 +7,42 @@ class TestInsertionSort(unittest.TestCase):
         initial_list = []
         x = 0
         expected_list = [0]
-        result = insertion_sort(initial_list, x)
+        result = ais.insertion_sort(initial_list, x)
         self.assertEqual(expected_list, result)
 
     def test_positive_integers_list(self):
         initial_list = [1, 2, 4]
         x = 3
         expected_list = [1, 2, 3, 4]
-        result = insertion_sort(initial_list, x)
+        result = ais.insertion_sort(initial_list, x)
         self.assertEqual(expected_list, result)
 
     def test_negative_integers_list(self):
         initial_list = [-3, 2, 8]
         x = -1
         expected_list = [-3, -1, 2, 8]
-        result = insertion_sort(initial_list, x)
+        result = ais.insertion_sort(initial_list, x)
         self.assertEqual(expected_list, result)
 
     def test_edge_case_end_of_list(self):
         initial_list = [1, 2, 3, 4, 5]
         x = 6
         expected_list = [1, 2, 3, 4, 5, 6]
-        result = insertion_sort(initial_list, x)
+        result = ais.insertion_sort(initial_list, x)
         self.assertEqual(expected_list, result)
 
     def test_edge_case_beginning_of_list(self):
         initial_list = [1, 2, 3, 4, 5]
         x = 0
         expected_list = [0, 1, 2, 3, 4, 5]
-        result = insertion_sort(initial_list, x)
+        result = ais.insertion_sort(initial_list, x)
         self.assertEqual(expected_list, result)
 
     def test_edge_case_number_exists_in_list(self):
         initial_list = [1, 2, 3, 4, 5]
         x = 4
         expected_list = [1, 2, 3, 4, 4, 5]
-        result = insertion_sort(initial_list, x)
+        result = ais.insertion_sort(initial_list, x)
         self.assertEqual(expected_list, result)
 
 
@@ -50,31 +50,31 @@ class TestSortWithInsertionSort(unittest.TestCase):
     def test_empty_list(self):
         initial_list = []
         expected_list = []
-        result = sort_with_insertion_sort(initial_list)
+        result = ais.sort_with_insertion_sort(initial_list)
         self.assertEqual(expected_list, result)
 
     def test_already_sorted_list(self):
         initial_list = [1, 2, 3]
         expected_list = [1, 2, 3]
-        result = sort_with_insertion_sort(initial_list)
+        result = ais.sort_with_insertion_sort(initial_list)
         self.assertEqual(expected_list, result)
 
     def test_mixed_integers_list(self):
         initial_list = [1, 5, 2]
         expected_list = [1, 2, 5]
-        result = sort_with_insertion_sort(initial_list)
+        result = ais.sort_with_insertion_sort(initial_list)
         self.assertEqual(expected_list, result)
 
     def test_edge_case_sort_whole_list(self):
         initial_list = [4, 3, 2, 1]
         expected_list = [1, 2, 3, 4]
-        result = sort_with_insertion_sort(initial_list)
+        result = ais.sort_with_insertion_sort(initial_list)
         self.assertEqual(expected_list, result)
 
     def test_edge_case_duplicate_integers_in_list(self):
         initial_list = [8, 2, 5, 3, 4, 5]
         expected_list = [2, 3, 4, 5, 5, 8]
-        result = sort_with_insertion_sort(initial_list)
+        result = ais.sort_with_insertion_sort(initial_list)
         self.assertEqual(expected_list, result)
 
 
