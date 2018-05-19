a7e1356a0e0f7a5f4bafde11ac71a389b272ec1b
German Om <germanom91@gmail.com>
2018-05-18 21:39:33 -0700

    adding some more test cases

--- tests/test_insertion_sort.py
+++ tests/test_insertion_sort.py
@@ -5,10 +5,24 @@ from algorithms.insertion_sort import insertion_sort
 class TestInsertionSort(unittest.TestCase):
     def test_empty_list(self):
         initial_list = []
+        x = 0
         expected_list = [0]
-        result = insertion_sort(initial_list, 0)
+        result = insertion_sort(initial_list, x)
         self.assertEqual(expected_list, result)
 
+    def test_positive_integers_list(self):
+        initial_list = [1, 2, 4]
+        x = 3
+        expected_list = [1, 2, 3, 4]
+        result = insertion_sort(initial_list, x)
+        self.assertEqual(expected_list, result)
+
+    def test_negative_integers_list(self):
+        initial_list = [-3, 2, 8]
+        x = -1
+        expected_list = [-3, -1, 2, 8]
+        result = insertion_sort(initial_list, x)
+        self.assertEqual(expected_list, result)
 
 if __name__ == '__main__':
     unittest.main()
