348b077be2961a754fd29cf83e07dc89b5e4754b
German Om <germanom91@gmail.com>
2018-05-19 17:45:37 -0700

    Renaming functions and tests for clarity

--- algorithms/insertion_sort/insertion_sort.py
+++ algorithms/insertion_sort/insertion_sort.py
@@ -1,31 +1,11 @@
 """
-Script that shows how to sort a list with insertion sort
+Functions to help explain how to sort a list with the insertion sort algorithm
 @author: German Om
-
-
-INSERTION SORT
-Suppose that we have a sorted array, A, and a single element outside of it, x. The natural question is, how can we place
-this element into our array while still keeping it sorted?
-
-This operation is known as an insertion, and there are multiple ways to do it.
-
--   Place x at the end of A.
--   Compare x to the element on its left. There are three possibilities.
-    -   If there is no element to the left, then we are done, since x will be the smallest element and it is already at
-        the start of the array.
-    -   If x is greater than or equal to it, then we are done; x is ahead of all smaller elements and behind all larger
-        elements, so the array is once again sorted.
-    -   If x is smaller, then it should be before the other element. Switch the two to put x on front.
--   Repeat step two until finished.
-
-With each repetition of step two, we will either finish inserting, or reduce the number of elements ahead of x by one.
-Since we are also finished when no more elements remain in front of x, repeating step two is guaranteed to successfully
-make an insertion.
 
 """
 
 
-def insertion_sort(input_list, x):
+def insertion(input_list, x):
     """
     Performs insertion sort and returns the new sorted array with x added to the list.
     :param input_list: sorted list of integers
@@ -44,7 +24,7 @@ def insertion_sort(input_list, x):
     return sorted_list
 
 
-def sort_with_insertion_sort(input_list):
+def sort_with_insertion(input_list):
     """
     Performs full insertion sort of any integer list and returns a sorted list
     :param input_list: integer list (sorted or not)
@@ -52,15 +32,22 @@ def sort_with_insertion_sort(input_list):
     """
     sorted_array = []
     for i in input_list:
-        sorted_array = insertion_sort(sorted_array, i)
+        sorted_array = insertion(sorted_array, i)
     return sorted_array
 
 
-def sort_with_insertion_sort_in_place(input_list):
+def insertion_sort(input_list):
+    """
+    Takes in a list of unsorted integers and performs insertion sort (in-place sorting) to return a sorted list
+    :param input_list:
+    :return:
+    """
     for i in xrange(1, len(input_list)):
         for j in xrange(i, 0, -1):
             if input_list[j - 1] < input_list[j]:
+                # order is sorted, so we can exit the loop!
                 break
             else:
+                # swapping values in-place
                 input_list[j], input_list[j - 1] = input_list[j - 1], input_list[j]
     return input_list
